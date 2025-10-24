f09a8289361d9a9fe30caf803ad862f63b69bc5f
phufaheng <phufah.eng@student.mahidol.edu>
2025-10-24 15:06:20 +0000

    done

--- sorting.h
+++ sorting.h
@@ -1,49 +1,75 @@
 // selection sort function module in C
 
-void display(int a[],int n){
-
+void display(int *a,int n){
     int i;
 
-    for(i=0;i<n;i++)
-        printf("%5d",a[i]);
-    printf("\n");
+    for(i=0; i<n; i++){
+        cout<<a[i]<<" ";
+    }
+    cout<<endl;
   
 }
 
-void selectionSort(int data[], int length) 
+void selection_sort(int *a, int n) 
 { 
-	int i, j, m, mi; 
-	
-     display(data,length);
-     
-	
+	int i, j;
+  int max, mi;
+    
+  //1 round first
+  for(j = 0; j < n-1; j++){
+    max = a[j];
+    mi = j;
+    
+    for(i = j+1; i < n; i++){
+      if(a[i] > max){
+        max = a[i];
+        mi = i;
+      }
+    }
+    swap(a[j],a[mi]);
+    display(a,n);
+  }
 } 
 
-
-
-void insertion(int a[],int n){
-
-
+void insertion_sort(int *a, int n){
+  int i, j, value;
+  for(i = 1; i < n; i++){
+    value = a[i];
+    for (j = i - 1; j >= 0 && a[j] < n; j--){
+      a[j + 1] = a[j];
+      display(a,n);
+    }
+    a[j + 1] = value;
+    display(a,n);
+  }
 }
 
-void swap(int *a,int *b){
- int temp;
-  temp=*a;
-  *a=*b;
-  *b=temp;
+
+void swap(int &a, int &b){
+  int temp;
+  temp = a;
+  a = b;
+  b = temp;
   
 }
 
-void bubbleSort(int a[],int n){
-
-int i,j;
-int sorted;
-// how may pair to compare?
-for(j=1;j<=n-1;j++){
+void bubble_sort(int *a, int n){
+  int i,j;
+  int sorted;
  
-  display(a,n);
- }
-  
+  for(i = 0; i < n-1; i++){
+    sorted = 1;
+    for(j = i; j < n-1-i; j++){
+      if(a[j] < a[j+1]){
+        swap(a[j], a[j+1]);
+        sorted = 0;
+      }
+      display(a,n);
+    }
+    if(sorted == 1){
+      break;
+    }
+  }
 }
 
 
